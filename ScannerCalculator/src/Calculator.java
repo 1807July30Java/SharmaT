19a1f2fbfa3885bed0afb78ee461ff55e1b7fd63
tusharshar10 <41876943+tusharshar10@users.noreply.github.com>
2018-08-03 12:02:48 -0400

    Add files via upload

---
+++ ScannerCalculator/src/Calculator.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+		public class Calculator {
+
+		    public static void main(String[] args) {
+
+		    	double a, b;
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.print("Enter first number:");
+
+		        
+		        a = scanner.nextDouble();
+		        System.out.print("Enter second number:");
+		        b = scanner.nextDouble();
+
+		        System.out.print("Enter an operator (+, -, *, /): ");
+		        char operator = scanner.next().charAt(0);
+
+		        scanner.close();
+		        double output;
+
+		        switch(operator)
+		        {
+		            case '+':
+		            	output = a + b;
+		                break;
+
+		            case '-':
+		            	output = a - b;
+		                break;
+
+		            case '*':
+		            	output = a * b;
+		                break;
+
+		            case '/':
+		            	output = a / b;
+		                break;
+
+		            default:
+		                System.out.printf("wrong operator");
+		                return;
+		        }
+
+		        System.out.println(a +" "+ operator +" "+ b +": "+ output);
+		    }
+			
+		
+		
+	}
+
